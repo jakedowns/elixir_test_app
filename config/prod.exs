c3a7f8f4214314304df010605d3bb8142887c876
jakedowns <mail@jakedowns.com>
2020-04-19 16:25:12 -0700

    add secrets file

--- config/prod.exs
+++ config/prod.exs
@@ -23,9 +23,9 @@ config :community, CommunityWeb.Endpoint,
 
 config :community, Community.Repo,
        adapter: Ecto.Adapters.Postgres,
-       url: {:system, "DATABASE_URL"},
+       url: System.get_env("DATABASE_URL"),
        database: "",
-       ssl: true,
+       ssl: false,
        pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
 
 # Do not print debug messages in production
