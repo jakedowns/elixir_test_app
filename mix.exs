11af5395d310bb6018b9aaac16b1f6a33d7becbf
Jake Downs <mail@jakedowns.com>
2020-04-19 18:07:30 -0700

    assets replaced

--- mix.exs
+++ mix.exs
@@ -33,9 +33,9 @@ defmodule Community.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.4"},
+      {:phoenix, "~> 1.4.16"},
       {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.2"},
+      {:phoenix_ecto, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:plug_cowboy, "~> 1.0"},
